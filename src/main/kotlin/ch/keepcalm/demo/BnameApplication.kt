70c607b4397903028b8a49fe8b0ed7feac4f508d
Marcel Widmer <marzelwidmer@gmail.com>
2020-08-19 17:40:22 +0200

    Initial commit
    Downgrde spring version
    
    Polishing.
    
    Configure JIB
    
    add missing maven profile
    
    add clean compile jib:build
    
    Add iamge name to jib build
    
    change to jib:dockerBuild
    
    fix: just a test
    
    Update pipeline
    
    Polishing.
    
    Update azure-pipelines.yml for Azure Pipelines
    
    Add Stage build
    
    Add Stage build

---
+++ src/main/kotlin/ch/keepcalm/demo/BnameApplication.kt
@@ -0,0 +1,13 @@
+package ch.keepcalm.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class BnameApplication
+
+fun main(args: Array<String>) {
+    runApplication<BnameApplication>(*args)
+}
+
