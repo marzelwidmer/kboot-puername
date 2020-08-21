591de1b67f086d9cd61beb90e957ba19c3f0233c
Marcel Widmer <marzelwidmer@gmail.com>
2020-08-21 13:12:23 +0200

    Remove old CI scripts

--- ci/current-version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-GIT_REV_LIST=`git rev-list --tags --max-count=1`
-GIT_REV_LAST=`git rev-parse HEAD`
-VERSION='0.0.0'
-if [[ -n $GIT_REV_LIST ]]; then
-    VERSION=`git describe --tags $GIT_REV_LIST`
-fi
-echo $VERSION
