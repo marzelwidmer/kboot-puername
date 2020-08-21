7469408b104e52a876fed591009682ebac42ea50
Marcel Widmer <marzelwidmer@gmail.com>
2020-08-21 13:51:34 +0200

    TDD exercise

---
+++ src/test/kotlin/ch/keepcalm/demo/IntegrationTest.kt
@@ -0,0 +1,21 @@
+package ch.keepcalm.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class IntegrationTest ( ) {
+
+
+
+	@Test
+	fun contextLoads() {
+		// Given
+
+		// When
+
+		// Then
+	}
+
+}
