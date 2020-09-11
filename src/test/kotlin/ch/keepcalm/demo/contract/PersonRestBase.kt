f4041d7bee758c466929e46ebc11a49b06ca78f3
Marcel Widmer <marzelwidmer@gmail.com>
2020-09-12 01:29:17 +0200

    try to get resdocs to fly

--- src/test/kotlin/ch/keepcalm/demo/contract/PersonRestBase.kt
+++ src/test/kotlin/ch/keepcalm/demo/contract/PersonRestBase.kt
@@ -3,14 +3,13 @@ package ch.keepcalm.demo.contract
 import ch.keepcalm.demo.person.PersonDocument
 import io.restassured.module.webtestclient.RestAssuredWebTestClient
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.TestInfo
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.http.MediaType
-import org.springframework.restdocs.RestDocumentationContextProvider
 import org.springframework.restdocs.RestDocumentationExtension
-import org.springframework.restdocs.operation.preprocess.Preprocessors
-import org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.documentationConfiguration
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.web.reactive.function.server.RouterFunction
@@ -19,45 +18,72 @@ import org.springframework.web.reactive.function.server.body
 import org.springframework.web.reactive.function.server.router
 import reactor.core.publisher.Flux
 
+
+//@WebFluxTest
+//@Import(value = [PersonRouter::class])
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureRestDocs(outputDir = "target/snippets")
+@AutoConfigureWebTestClient
+@AutoConfigureJsonTesters
+@DirtiesContext
 @ExtendWith(RestDocumentationExtension::class, SpringExtension::class)
-@AutoConfigureRestDocs
 abstract class PersonRestBase {
 
     @BeforeEach
-    fun setup(restDocumentation: RestDocumentationContextProvider?, testInfo: TestInfo) {
-        RestAssuredWebTestClient
-            .webTestClient(webTestClient2(restDocumentation))
+    fun setup() {
+        RestAssuredWebTestClient.webTestClient(WebTestClient.bindToRouterFunction(
+            apiPersons()
+        ).build())
     }
-
-
-    private fun webTestClient(restDocumentation: RestDocumentationContextProvider?) = WebTestClient
-        .bindToRouterFunction(apiPersons())
-        .configureClient()
-        .filter(documentationConfiguration(restDocumentation))
-        .build()
-
-    private fun webTestClient2(restDocumentation: RestDocumentationContextProvider?) = WebTestClient
-        .bindToRouterFunction(apiPersons())
-        .configureClient().baseUrl("https://api.example.com")
-        .filter(documentationConfiguration(restDocumentation))
-        .build()
-
-    private fun webTestClient3(restDocumentation: RestDocumentationContextProvider?) = WebTestClient
-        .bindToRouterFunction(apiPersons())
-        .configureClient()
-        .baseUrl("https://api.example.com")
-        .filter(documentationConfiguration(restDocumentation)
-            .operationPreprocessors()
-            .withRequestDefaults(Preprocessors.prettyPrint())
-            .withResponseDefaults(Preprocessors.prettyPrint()))
-        .build()
 
     private fun apiPersons(): RouterFunction<ServerResponse> = router {
-        "api".nest {
-            GET("/persons") {
-                ok().contentType(MediaType.APPLICATION_JSON)
-                    .body<Any>(Flux.just(PersonDocument(firstName = "John")))
+                "api".nest {
+                    GET("/persons") {
+                        ok().contentType(MediaType.APPLICATION_JSON)
+                            .body<Any>(Flux.just(PersonDocument(firstName = "John")))
+                    }
+                }
             }
-        }
-    }
 }
+//@ExtendWith(RestDocumentationExtension::class, SpringExtension::class)
+//@AutoConfigureRestDocs
+//abstract class PersonRestBase {
+//
+//    @BeforeEach
+//    fun setup(restDocumentation: RestDocumentationContextProvider?, testInfo: TestInfo) {
+//        RestAssuredWebTestClient
+//            .webTestClient(webTestClient2(restDocumentation))
+//    }
+//
+//
+//    private fun webTestClient(restDocumentation: RestDocumentationContextProvider?) = WebTestClient
+//        .bindToRouterFunction(apiPersons())
+//        .configureClient()
+//        .filter(documentationConfiguration(restDocumentation))
+//        .build()
+//
+//    private fun webTestClient2(restDocumentation: RestDocumentationContextProvider?) = WebTestClient
+//        .bindToRouterFunction(apiPersons())
+//        .configureClient().baseUrl("https://api.example.com")
+//        .filter(documentationConfiguration(restDocumentation))
+//        .build()
+//
+//    private fun webTestClient3(restDocumentation: RestDocumentationContextProvider?) = WebTestClient
+//        .bindToRouterFunction(apiPersons())
+//        .configureClient()
+//        .baseUrl("https://api.example.com")
+//        .filter(documentationConfiguration(restDocumentation)
+//            .operationPreprocessors()
+//            .withRequestDefaults(Preprocessors.prettyPrint())
+//            .withResponseDefaults(Preprocessors.prettyPrint()))
+//        .build()
+//
+//    private fun apiPersons(): RouterFunction<ServerResponse> = router {
+//        "api".nest {
+//            GET("/persons") {
+//                ok().contentType(MediaType.APPLICATION_JSON)
+//                    .body<Any>(Flux.just(PersonDocument(firstName = "John")))
+//            }
+//        }
+//    }
+//}
